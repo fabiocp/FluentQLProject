3a9b9579860b1d81957f1f662459653c3b21c9b9
Fabio Cesar Pasquali <fabio.pasquali@questores.com.br>
2016-12-17 19:01:58 -0200

    sss

--- FluentQL.Firebird.Tests/GeneralTests.cs
+++ FluentQL.Firebird.Tests/GeneralTests.cs
@@ -105,5 +105,27 @@ namespace FluentQL.Firebird.Tests {
         }
 
 
+        [TestMethod]
+        public void TestCompleto() {
+
+            var expressao = new QLExpr("nome", QLOperation.Contem, "fab")
+                .And(new QLExpr("filtrovalor", QLOperation.MaiorIgual, 5.2).Or("filtrofixo=2"))
+                .And("filtroCustomizado", 1);
+
+
+            expressao
+                .Rename("filtrovalor", "valor")
+                .DefinirExpressaoCustom<int>("filtroCustomizado", valor =>
+                    "exists(select 1 from estudante e where e.idpessoa=" + valor.ToString() + ")");
+
+            new FirebirdFluentQLBuilder()
+                .Gerar(expressao)
+                .ShouldEqual("(nome like '%fab%') and ((valor >= 5.2) or (filtrofixo=2)) and (exists(select 1 from estudante e where e.idpessoa=1))");
+
+            
+
+        }
+
+
     }
 }
